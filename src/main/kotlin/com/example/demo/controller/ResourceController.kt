d4c856b558cc9e77777288465979f763e8a3d3b6
aslivets <slivets.aleksay@gmail.com>
2019-07-27 14:39:22 +0200

    - add sorting by request parameter

--- src/main/kotlin/com/example/demo/controller/ResourceController.kt
+++ src/main/kotlin/com/example/demo/controller/ResourceController.kt
@@ -2,21 +2,24 @@ package com.example.demo.controller
 
 import com.example.demo.config.Const.CACHE_AGE_IN_SEC
 import com.example.demo.model.Resource
+import com.example.demo.repository.ResourceDao
+import org.springframework.data.domain.Sort
 import org.springframework.http.CacheControl
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.TimeUnit
 
 @RestController
-class ResourceController {
+class ResourceController(val repository: ResourceDao) {
 
     @RequestMapping("/v1/resources")
-    fun payment(): ResponseEntity<Resource> {
-        val resource = Resource("123", "some value", 9000, "12:00:00")
+    fun resources(@RequestParam("order", required = false, defaultValue = "ASC") order: String): ResponseEntity<MutableIterable<Resource>> {
+        val resources = repository.findAll(Sort.by(Sort.Direction.fromString(order), "id"))
         return ResponseEntity.ok()
                 .cacheControl(CacheControl.maxAge(CACHE_AGE_IN_SEC, TimeUnit.SECONDS))
-                .eTag(resource.hashCode().toString())
-                .body(resource)
+                .eTag(resources.hashCode().toString())
+                .body(resources)
     }
 }
